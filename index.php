3096fce24ccd8a71b2e30ea51f8cdd3f29ca4733
cis <cis@CISM227.(none)>
2013-12-24 16:28:30 +0530

    added initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$arr=Array
+(
+    [0] => stdClass Object
+        (
+            [room_price] => 200
+        )
+
+    [1] => stdClass Object
+        (
+            [room_price] => 500 
+        )
+
+    [2] => stdClass Object
+        (
+            [room_price] => 610.0
+        )
+
+    [3] => stdClass Object
+        (
+            [room_price] => 1000
+        )
+
+);
+echo "<pre>".print_r($arr);
+?>
